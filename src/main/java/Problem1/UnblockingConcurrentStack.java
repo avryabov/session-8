328ed9f2aaf94fcd5a9ef9d4b15ea48f407e456b
avryabov <antonrbv@gmail.com>
2018-04-23 20:28:26 +0300

    session-8, problem1 done

---
+++ src/main/java/Problem1/UnblockingConcurrentStack.java
@@ -0,0 +1,62 @@
+package Problem1;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ */
+public class UnblockingConcurrentStack<E> {
+
+    private final AtomicReference<Node<E>> top = new AtomicReference<>(null);
+
+    public E push(E item) {
+        Node<E> newTop = new Node<>(item, null);
+        while (true) {
+            Node<E> oldTop = top.get();
+            newTop.next = oldTop;
+            if (top.compareAndSet(oldTop, newTop)) {
+                return item;
+            }
+        }
+    }
+
+    public E pop() {
+        while(true) {
+            Node<E> oldTop = top.get();
+            Node<E> newTop = oldTop.next;
+            if(top.compareAndSet(oldTop, newTop))
+                return oldTop.item;
+        }
+    }
+
+    public E peek() {
+        return top.get().item;
+    }
+
+    public boolean empty() {
+        if(top.get() == null)
+            return true;
+        else
+            return false;
+    }
+
+    public int search(E o) {
+        int count = 0;
+        for (Node<E> node = top.get(); node != null; node = node.next) {
+            E item = node.item;
+            if (item.equals(o))
+                return count;
+            count++;
+        }
+        return -1;
+    }
+
+    static class Node<E> {
+        private final E item;
+        private Node<E> next;
+
+        public Node(E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
